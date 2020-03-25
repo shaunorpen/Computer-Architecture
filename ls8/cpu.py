77184d8636250f55c1f5c4e2b57c512f7a011205
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-25 13:38:28 +0000

    add comments to branch table

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -20,19 +20,73 @@ class CPU:
         self.instructions[0b01000111] = 'PRN'
         
         self.branch_table = dict()
-        self.branch_table['LDI'] = self.ldi
-        self.branch_table['MUL'] = self.mul
-        self.branch_table['PRN'] = self.prn
-        self.branch_table['HLT'] = self.hlt
+        """
+        ALU ops
+        ADD  10100000 00000aaa 00000bbb
+        SUB  10100001 00000aaa 00000bbb
+        MUL  10100010 00000aaa 00000bbb
+        DIV  10100011 00000aaa 00000bbb
+        MOD  10100100 00000aaa 00000bbb
 
+        INC  01100101 00000rrr
+        DEC  01100110 00000rrr
+
+        CMP  10100111 00000aaa 00000bbb
+
+        AND  10101000 00000aaa 00000bbb
+        NOT  01101001 00000rrr
+        OR   10101010 00000aaa 00000bbb
+        XOR  10101011 00000aaa 00000bbb
+        SHL  10101100 00000aaa 00000bbb
+        SHR  10101101 00000aaa 00000bbb
+        """
+        self.branch_table['MUL'] = self.mul
+        """
+        PC Mutators
+        CALL 01010000 00000rrr
+        RET  00010001
+
+        INT  01010010 00000rrr
+        IRET 00010011
+
+        JMP  01010100 00000rrr
+        JEQ  01010101 00000rrr
+        JNE  01010110 00000rrr
+        JGT  01010111 00000rrr
+        JLT  01011000 00000rrr
+        JLE  01011001 00000rrr
+        JGE  01011010 00000rrr
+        """
+
+        """
+        Other
+        NOP  00000000
+
+        HLT  00000001 
+
+        LDI  10000010 00000rrr iiiiiiii
+
+        LD   10000011 00000aaa 00000bbb
+        ST   10000100 00000aaa 00000bbb
+
+        PUSH 01000101 00000rrr
+        POP  01000110 00000rrr
+
+        PRN  01000111 00000rrr
+        PRA  01001000 00000rrr
+        """
+        self.branch_table['HLT'] = self.hlt
+        self.branch_table['LDI'] = self.ldi
+        self.branch_table['PRN'] = self.prn
+
+    def mul(self, operand_a, operand_b):
+        self.alu('MUL', operand_a, operand_b)
+    
     def hlt(self):
         self.running = False
 
     def ldi(self, operand_a, operand_b):
         self.reg_write(operand_a, operand_b)
-
-    def mul(self, operand_a, operand_b):
-        self.alu('MUL', operand_a, operand_b)
 
     def prn(self, operand_a):
         print(self.reg_read(operand_a))
